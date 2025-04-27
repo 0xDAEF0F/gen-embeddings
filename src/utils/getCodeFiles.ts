4cdbfa76bf525f481e6cbc26d86ebb9023208578
Alex T <aletapia@proton.me>
2025-04-27 14:20:47 -0600

    Update ts-morph to version 25.0.1 and enhance getCodeFiles function to filter out test and generated files.

--- src/utils/getCodeFiles.ts
+++ src/utils/getCodeFiles.ts
@@ -4,5 +4,13 @@ export const getCodeFiles = (codebasePath: string): SourceFile[] => {
 	const project = new Project({
 		tsConfigFilePath: `${codebasePath}/tsconfig.json`,
 	});
-	return project.getSourceFiles();
+	const files = project.getSourceFiles();
+
+	// TODO: make this configurable in a parameter for the function
+	return files.filter((file) => {
+		const fileName = file.getFilePath();
+		if (fileName.includes(".test.ts")) return false;
+		if (fileName.includes("__generated__")) return false;
+		return true;
+	});
 };
